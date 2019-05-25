7767aac43025be1ee8f26ccb83ebb59ac6eda4db
Will Friebel <will.friebel@uber.com>
2019-05-25 14:55:07 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer");
+const argv = require("minimist")(process.argv.slice(2));
+
+const links = [
+  "https://psc-software.atlassian.net/wiki/spaces/flyingpdf/pdfpageexport.action?pageId=1773289",
+  "https://psc-software.atlassian.net/wiki/spaces/flyingpdf/pdfpageexport.action?pageId=1773289",
+  "https://psc-software.atlassian.net/wiki/spaces/flyingpdf/pdfpageexport.action?pageId=1773289"
+];
+
+(async () => {
+  try {
+    console.log("Connecting to the browser");
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: argv.browser
+    });
+    const page = await browser.newPage();
+
+    console.log("Downloading PDFs\n");
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      await page.goto(link);
+      await page.waitForSelector("#taskCurrentStatus > a");
+      console.log(link);
+      await page.click("#taskCurrentStatus > a");
+    }
+
+    console.log(
+      "\nDownload complete. All downloaded PDFs can be found in your downloads folder."
+    );
+    console.log("Type control + c to kill this program");
+  } catch (error) {
+    console.log("An error occured while running this script");
+    console.log("Error:", error);
+  }
+})();
